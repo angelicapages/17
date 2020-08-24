b0287ad233ff1b1a6fc7ccfa3f91193049afab24
angelicapages <angelicapages@gmail.com>
2020-08-24 20:25:59 -0300

    primer ejercicio

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log (document)
+
+const elementosConLaEtiquetaP = document.getElementsByTagName ("p")
+const elelemtosConLaClaseVerde = document.getElementsByClassName ("verde")
+const elementosConElIDBordeRojo = document.getElementById ("borde-rojo")
+
+console.log (elementosConElIDBordeRojo)
+console.log (elelemtosConLaClaseVerde)
+console.log (elementosConLaEtiquetaP)
