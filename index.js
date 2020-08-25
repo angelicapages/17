d575c82dde86985fea63668a700e02da6670b537
angelicapages <angelicapages@gmail.com>
2020-08-24 21:19:43 -0300

    hay problemas con remove

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 console.log (document)
 
-const elementosConLaEtiquetaP = document.getElementsByTagName ("p")
-const elelemtosConLaClaseVerde = document.getElementsByClassName ("verde")
-const elementosConElIDBordeRojo = document.getElementById ("borde-rojo")
+//const elementosConLaEtiquetaP = document.getElementsByTagName ("p")
+//const elelemtosConLaClaseVerde = document.getElementsByClassName ("verde")
+//const elementosConElIDBordeRojo = document.getElementById ("borde-rojo")
 
-console.log (elementosConElIDBordeRojo)
-console.log (elelemtosConLaClaseVerde)
-console.log (elementosConLaEtiquetaP)
+//console.log (elementosConElIDBordeRojo)
+//console.log (elelemtosConLaClaseVerde)
+//console.log (elementosConLaEtiquetaP)
+
+const parrafoVerde = document.getElementsByClassName ("verde")
+parrafoVerde.classlist.remove("verde")
+
+const parrafoRojo = document.getElementsByClassName ("rojo")
+parrafoRojo.classlist.add("rosa")
+
+const parrafoRosa = document.getElementsByClassName ("rosa")
+parrafoRosa.classlist.toggle("rosa")
+
+const parrafoAmarillo =document.getElementsByClassName ("amarillo")
+console.log (parrafoAmarillo.classlist.contains ("rojo"))
